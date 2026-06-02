20f403f14401840c3444ad72cc569ed2273e2780
areebmohd <areebmohd683@gmail.com>
2026-06-02 17:59:19 +0530

    update 1.0.6

--- App.tsx
+++ App.tsx
@@ -19,7 +19,7 @@ import { supabase } from './src/api/supabase';
 export const navigationRef = createNavigationContainerRef();
 
 // IMPORTANT: Update this manually on every release to match versionCode in android/app/build.gradle
-const CURRENT_VERSION_CODE = 6;
+const CURRENT_VERSION_CODE = 7;
 
 function App() {
   return (
